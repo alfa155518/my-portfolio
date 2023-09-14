40bf072399be046b161f3e0d5cbfb5f34749d306
Ahmed Hassop <alfaelkanas@gmail.com>
2023-09-14 16:10:21 +0300

    modified js

--- js/script.js
+++ js/script.js
@@ -224,7 +224,7 @@ navigatingBullet.forEach((bullet) => {
   });
 });
 
-Start disable Inspect
+// Start disable Inspect
 window.addEventListener("contextmenu", (e) => {
   e.preventDefault();
   alert("Don't be one of those who copy and paste");
