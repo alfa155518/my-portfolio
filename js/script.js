3b9167f31855bfc301fa5e8b702b9b31bcbac212
Ahmed Hassop <alfaelkanas@gmail.com>
2023-09-16 15:20:25 +0300

    update css file

--- js/script.js
+++ js/script.js
@@ -226,7 +226,7 @@ navigatingBullet.forEach((bullet) => {
 
 
 // Start disable Inspect
-window.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-  alert("Don't be one of those who copy and paste");
-});
+// window.addEventListener("contextmenu", (e) => {
+//   e.preventDefault();
+//   alert("Don't be one of those who copy and paste");
+// });
